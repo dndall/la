1a9bea10ea3a4955a3fe9146e3ca672deac5231f
Aashish <71197410+dndall@users.noreply.github.com>
2020-09-14 16:51:44 +0530

    TRC20

--- TRC20.sol
+++ TRC20.sol
@@ -1,6 +1,7 @@
 pragma solidity ^0.4.16;
 
-interface tokenRecipient { function receiveApproval(address _from, uint256 _value, address _token, bytes _extraData) external; }
+import "./ITRC20.sol";
+import "./SafeMath.sol";
 
 contract TRC20 is ITRC20 {
     // Public variables of the token
